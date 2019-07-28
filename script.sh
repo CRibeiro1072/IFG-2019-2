928229b4b4032550ec8f77e75eac3e97ad36956d
CRibeiro1072 <claudinei.ribeiro.jatai.go@gmail.com>
2019-07-28 19:47:18 -0300

    Modifiquei alguma coisa

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+clear;
+echo"Ola cheguei...";
+exit;
+/*Comandos usados no github em linux via terminal*/
+git config --global user.name "CRibeiro1072";
+/**/
+git config --global user.email "claudinei.ribeiro.jatai.go@gmail.com";
+/**/
+git clone https://github.com/CRibeiro1072/2019-1-PI-III
+/**/
+git add arquivo_x
+/**/
+git commit -m "Adicionado um arquivo script.sh"
+/**/
+git push
